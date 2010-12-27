37ee996ed3fe25273ecde1d7f39adcd573895fd8
Amalia <amalia@linux.lol>
2010-12-28 01:57:21 +0200

    Added a 'Solution.xml' generating script (using the actual-work SED script...)

---
+++ makeSolutionXML.bash
@@ -0,0 +1,11 @@
+#!/bin/dash
+
+if [ -z "$1" ]
+	then	echo 'Give the solution filepath'
+		exit 1
+fi
+
+root_name='VisualStudioSolution'
+echo "$<$root_name>"
+sed --regexp-extended --file 'deltaide2make/vcsol2xml.sed' "$1"
+echo "</$root_name>"
