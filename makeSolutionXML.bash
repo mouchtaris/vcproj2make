f79d89189696ebcf162e719c87917a6550b8d9b9
Amalia <amalia@linux.lol>
2010-12-28 01:58:59 +0200

    immediate creation bug fix...

--- makeSolutionXML.bash
+++ makeSolutionXML.bash
@@ -6,6 +6,6 @@ if [ -z "$1" ]
 fi
 
 root_name='VisualStudioSolution'
-echo "$<$root_name>"
+echo "<$root_name>"
 sed --regexp-extended --file 'deltaide2make/vcsol2xml.sed' "$1"
 echo "</$root_name>"
