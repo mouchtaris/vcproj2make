302979b3afb8a5d6ac155b87d232a135cf9b3605
Nikos Mouchtaris <mouchtaris@gmail.com>
2014-09-01 19:43:08 +0300

    Initial j2dm project branch

--- vcproj2make_old/vcproj2make_testprojects/isidll/Include/isi/f.h
+++
@@ -1,9 +0,0 @@
-#include "isi/dll.h"
-
-namespace isi {
-
-    // f(x) = x + ISIDLL_F_ADJ
-    ISI_FUNC int f(int) throw();
-    ISI_FUNC int fadj(void) throw();
-
-} // namespace isi
--- vcproj2make_old/vcproj2make_testprojects/isidll/Include/main4.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MAIN4_H__
-#define __MAIN4_H__
-
-#include "isi/dll.h"
-
-ISI_CFUNC int main4 (int, char**) throw();
-
-#endif // __MAIN4_H__
